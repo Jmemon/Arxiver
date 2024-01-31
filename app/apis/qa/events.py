176e5a0c90b6d6c11848851717c0ad5802b5d1ad
Jmemon <johnpmemon@gmail.com>
2024-01-30 22:27:40 -0500

    Streaming Working

--- app/apis/qa/events.py
+++ app/apis/qa/events.py
@@ -1,4 +1,5 @@
 
+import eventlet
 from flask_socketio import emit
 
 from app import socketio
@@ -18,5 +19,7 @@ def handle_message(message):
     prompt = message.get('prompt')
     if prompt:
         for chunk in stream_llm_output(prompt):
-            print(chunk)
             emit('response', {'data': chunk}, namespace='/qa')
+            # This forces the server to take control for a brief second so the LLM isn't taking up
+            # all processing power. Otherwise all tokens are emitted after they're all generated
+            eventlet.sleep(0)
--- chat.py
+++ chat.py
@@ -9,3 +9,5 @@ app = create_app()
 
 if __name__ == '__main__':
     socketio.run(app, '127.0.0.1', 8080, log_output=True)
+    #socketio.run(app, '192.168.1.5', 8080, log_output=True)
+    #app.run('0.0.0.0', 8080)  # to find IP address
