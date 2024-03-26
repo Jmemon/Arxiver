783f856580d528c19abd5aa34efa2bf9fa4b73d3
Jmemon <johnpmemon@gmail.com>
2024-03-26 15:44:45 -0400

    Dockerfile

--- chat.py
+++ chat.py
@@ -8,6 +8,6 @@ app = create_rag_chain_eval_app()
 
 
 if __name__ == '__main__':
-    socketio.run(app, '127.0.0.1', 8080, log_output=True)
+    socketio.run(app, '0.0.0.0', 5000, log_output=True)
     #socketio.run(app, '192.168.1.5', 8080, log_output=True)
     #app.run('0.0.0.0', 8080)  # to find IP address
