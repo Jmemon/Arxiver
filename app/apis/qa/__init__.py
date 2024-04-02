83e1b32032ae6bade54174a74bd16bb4aa30b0ed
Jmemon <johnpmemon@gmail.com>
2024-04-02 15:37:56 -0400

    rag chain used for qa endpoint

--- app/apis/qa/__init__.py
+++ app/apis/qa/__init__.py
@@ -2,4 +2,11 @@ from flask import Blueprint
 
 qa_blueprint = Blueprint('qa_api', __name__)
 
+from qa_chain.pipeline import get_simple_rag_chain
+from utils import ARXIVER_PATH
+
+pipeline = get_simple_rag_chain(
+    [ARXIVER_PATH / 'papers' / 'mixtral_of_experts.pdf'], 
+    'mixtral_of_experts')
+
 from . import routes, events
